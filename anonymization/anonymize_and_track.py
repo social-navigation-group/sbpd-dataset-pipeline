d78149c3385973dcefed39d27130dae536a7cf7a
Allan Wang <allanwangliqian@gmail.com>
2025-03-19 19:04:39 +0900

    minor fixes (traj smooth) to anonymize and track

--- anonymization/anonymize_and_track.py
+++ anonymization/anonymize_and_track.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import toml
 import copy
+import time
 from ultralytics import YOLO
 
 def get_args():
@@ -21,7 +22,8 @@ def get_args():
 
     # Tracking parameters
     parser.add_argument("--iou", default=0.8, type=float, help="IoU threshold for tracking")
-    parser.add_argument("--tracker", default="bytetrack.yaml", type=str, help="Tracker configuration file")
+    parser.add_argument("--tracker", default="bytetrack.yaml", type=str, help="Tracker configuration file") # No need to download this yaml
+    parser.add_argument("--smooth-len", default=7, type=int, help="Length of the smoothing window")
 
     # Trajectory options
     parser.add_argument("--traj-fps", default=10, type=int, help="Fps of the trajectories")
@@ -43,16 +45,17 @@ def main(args):
     model = YOLO(args.model)
 
     for video_file in os.listdir(args.video):
-        if (not video_file.endswith(".mp4")) and (not video_file.endswith(".avi")):
+        video_test_file = video_file.lower()
+        if (not video_test_file.endswith(".mp4")) and (not video_test_file.endswith(".avi")):
             continue
 
         # Process paths
         video_path = os.path.join(args.video, video_file)
         output_path = os.path.join(args.output, video_file)
-        if video_file.endswith(".mp4"):
-            traj_fname = video_file.replace(".mp4", ".toml")
+        if video_test_file.endswith(".mp4"):
+            traj_fname = video_test_file.replace(".mp4", ".toml")
         else:
-            traj_fname = video_file.replace(".avi", ".toml")
+            traj_fname = video_test_file.replace(".avi", ".toml")
         trajectory_path = os.path.join(args.trajectory_output, traj_fname)
         trajectory_video_path = os.path.join(args.trajectory_output, video_file)
 
@@ -64,10 +67,11 @@ def main(args):
 
         frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = cap.get(cv2.CAP_PROP_FPS)
+        fps = int(round(cap.get(cv2.CAP_PROP_FPS)))
         num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         if (fps % args.traj_fps) != 0:
             print(f"ERROR: The video {video_path} has a frame rate that is not a multiple of the trajectory frame rate.")
+            print(f"Current frame rate: {fps}, trajectory frame rate: {args.traj_fps}")
             continue
         interval = max(1, int(fps / args.traj_fps))
 
@@ -79,6 +83,7 @@ def main(args):
         frame_id = 0
         trajectory_dict = {}
         id_list = []
+        start_time = time.time()
         print(f"Start processing video: {video_file}")
         while cap.isOpened():
             ret, frame = cap.read()
@@ -111,14 +116,15 @@ def main(args):
                         object_id = int(box.id[0]) if box.id is not None else -1
                         human_name = f"human{object_id}"
 
-                        if (object_id != -1) and (object_id not in id_list):
-                            id_list.append(object_id)
-                            trajectory_dict[human_name] = {}
-                            trajectory_dict[human_name]["traj_start"] = frame_id
-                            trajectory_dict[human_name]["trajectories"] = [coordinate]
-                            trajectory_dict[human_name]["human_context"] = None
-                        else:
-                            trajectory_dict[human_name]["trajectories"].append(coordinate)
+                        if (object_id != -1):
+                            if (object_id not in id_list):
+                                id_list.append(object_id)
+                                trajectory_dict[human_name] = {}
+                                trajectory_dict[human_name]["traj_start"] = frame_id // interval
+                                trajectory_dict[human_name]["trajectories"] = [coordinate]
+                                trajectory_dict[human_name]["human_context"] = None
+                            else:
+                                trajectory_dict[human_name]["trajectories"].append(coordinate)
             
             if args.blur_all:
                 save_frame = cv2.GaussianBlur(save_frame, (args.blur_size, args.blur_size), 0)
@@ -127,6 +133,22 @@ def main(args):
                 trajectory_video.write(save_frame)
             output.write(save_frame)
 
+        # smooth trajectories
+        smooth_len = args.smooth_len // 2
+        for human_name in trajectory_dict:
+            trajectory = trajectory_dict[human_name]["trajectories"]
+            smooth_trajectory = []
+            for i in range(len(trajectory)):
+                x = 0
+                y = 0
+                count = 0
+                for j in range(max(0, i - smooth_len), min(len(trajectory), i + smooth_len)):
+                    x += trajectory[j][0]
+                    y += trajectory[j][1]
+                    count += 1
+                smooth_trajectory.append([x / count, y / count])
+            trajectory_dict[human_name]["trajectories"] = smooth_trajectory
+
         with open(trajectory_path, 'w') as f:
             toml.dump(trajectory_dict, f)
 
@@ -134,6 +156,8 @@ def main(args):
         trajectory_video.release()
         cap.release()
         print(f"\nFinished processing video: {video_file}")
+        end_time = time.time()
+        print(f"Time elapsed: {end_time - start_time:.2f} seconds")
 
     return
 
