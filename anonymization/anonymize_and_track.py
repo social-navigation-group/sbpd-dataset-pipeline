47ada3afaefb72336eb028f8c2fee36c2caa6b51
Allan Wang <allanwangliqian@gmail.com>
2025-03-21 19:11:41 +0900

    annotation area tool + annotation area filter implemented

--- anonymization/anonymize_and_track.py
+++ anonymization/anonymize_and_track.py
@@ -2,9 +2,13 @@ import cv2
 import argparse
 import os
 import toml
+import yaml
 import copy
 import time
+import numpy as np
 from ultralytics import YOLO
+
+from define_area import select_area
 
 def get_args():
     parser = argparse.ArgumentParser(description="Anonymize a video using YOLO tracking")
@@ -13,14 +17,20 @@ def get_args():
     parser.add_argument("--video", default="./videos", type=str, help="Path to the folder that contains video files")
     parser.add_argument("--output", default="./videos_anonymized", type=str, help="Path to the folder that contains anonymized video files")
     parser.add_argument("--trajectory-output", default="./trajectories", type=str, help="Path to save the automated trajectories")
+    parser.add_argument("--area-path", default="./areas", type=str, help="Path to the area files")
 
     # Anonymization options
+    parser.add_argument("--no-blur", action="store_true", help="Do not anonymize the video (highest priority argument)")
     parser.add_argument("--blur-all", action="store_true", help="Blur entire video frames")
     parser.add_argument("--blur-black", action="store_true", help="Paint everything black")
     parser.add_argument("--blur-size", default=41, type=int, help="Size of the blur kernel")
     parser.add_argument("--blur-pct", default=0.5, type=float, help="Percentage of the bounding box to blur")
+    parser.add_argument("--blur-min", default=25, type=int, help="Minimum pixels of the bounding box to blur")
 
     # Tracking parameters
+    parser.add_argument("--no-track", action="store_true", help="Do not track objects")
+    parser.add_argument("--persist", action="store_true", help="Persist tracking")
+    parser.add_argument("--restrict-area", action="store_true", help="Restrict tracking to a specific area")
     parser.add_argument("--iou", default=0.8, type=float, help="IoU threshold for tracking")
     parser.add_argument("--tracker", default="bytetrack.yaml", type=str, help="Tracker configuration file") # No need to download this yaml
     parser.add_argument("--smooth-len", default=7, type=int, help="Length of the smoothing window")
@@ -35,10 +45,25 @@ def get_args():
     if not os.path.exists(args.video):
         print(f"ERROR: The video folder {args.video} does not exist.")
         exit(1)
+    if not os.path.isdir(args.video):
+        print(f"ERROR: The video path {args.video} is not a folder.")
+        exit(1)
     if not os.path.exists(args.output):
         os.makedirs(args.output)
+    elif not os.path.isdir(args.output):
+        print(f"ERROR: The output path {args.output} is not a folder.")
+        exit(1)
     if not os.path.exists(args.trajectory_output):
         os.makedirs(args.trajectory_output)
+    elif not os.path.isdir(args.trajectory_output):
+        print(f"ERROR: The trajectory output path {args.trajectory_output} is not a folder.")
+        exit(1)
+    if args.restrict_area:
+        if not os.path.exists(args.area_path):
+            os.makedirs(args.area_path)
+        elif not os.path.isdir(args.area_path):
+            print(f"ERROR: The area path {args.area_path} is not a folder.")
+            exit(1)
     return args
 
 def main(args):
@@ -51,13 +76,19 @@ def main(args):
 
         # Process paths
         video_path = os.path.join(args.video, video_file)
-        output_path = os.path.join(args.output, video_file)
-        if video_test_file.endswith(".mp4"):
-            traj_fname = video_test_file.replace(".mp4", ".toml")
-        else:
-            traj_fname = video_test_file.replace(".avi", ".toml")
-        trajectory_path = os.path.join(args.trajectory_output, traj_fname)
-        trajectory_video_path = os.path.join(args.trajectory_output, video_file)
+
+        if not args.no_blur:
+            output_path = os.path.join(args.output, video_file)
+        if not args.no_track:
+            if video_test_file.endswith(".mp4"):
+                traj_fname = video_test_file.replace(".mp4", ".toml")
+            else:
+                traj_fname = video_test_file.replace(".avi", ".toml")
+            trajectory_path = os.path.join(args.trajectory_output, traj_fname)
+            trajectory_video_path = os.path.join(args.trajectory_output, video_file)
+        if args.restrict_area:
+            area_fname = traj_fname.replace(".toml", ".yaml")
+            area_path = os.path.join(args.area_path, area_fname)
 
         # Initialize videos
         cap = cv2.VideoCapture(video_path)
@@ -76,8 +107,10 @@ def main(args):
         interval = max(1, int(fps / args.traj_fps))
 
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        output = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
-        trajectory_video = cv2.VideoWriter(trajectory_video_path, fourcc, args.traj_fps, (frame_width, frame_height))
+        if not args.no_blur:
+            output = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+        if not args.no_track:
+            trajectory_video = cv2.VideoWriter(trajectory_video_path, fourcc, args.traj_fps, (frame_width, frame_height))
 
         # Main loop
         frame_id = 0
@@ -96,14 +129,26 @@ def main(args):
             results = model.track(frame, persist = True, iou = args.iou, show = False, tracker = args.tracker, verbose = False)
             save_frame = copy.deepcopy(frame)
 
+            # load or create restrict areas
+            if (frame_id == 1) and args.restrict_area:
+                if os.path.exists(area_path):
+                    with open(area_path, 'r') as f:
+                        area = yaml.load(f, Loader=yaml.FullLoader)
+                else:
+                    # Use matplotlib to select points until right click TODO
+                    area = select_area(frame)
+                    with open(area_path, 'w') as f:
+                        yaml.dump(area, f)
+                area = np.array(area, np.int32)
+
             for box in results[0].boxes:
                 if box.cls[0] == 0:  # Only track humans (label 0)
                     bbox = box.xyxy[0].cpu().numpy()
                     x1, y1, x2, y2 = bbox
 
                     # Anonymize bounding box
-                    if not args.blur_all:
-                        new_y2 = y1 + (y2 - y1) * args.blur_pct
+                    if (not args.blur_all) and (not args.no_blur):
+                        new_y2 = y1 + max((y2 - y1) * args.blur_pct, args.blur_min)
                         x1, y1, x2, new_y2 = map(int, [x1, y1, x2, new_y2])
                         if args.blur_black:
                             save_frame[y1:new_y2, x1:x2, :] = 0
@@ -111,12 +156,12 @@ def main(args):
                             save_frame[y1:new_y2, x1:x2] = cv2.GaussianBlur(frame[y1:new_y2, x1:x2], (args.blur_size, args.blur_size), 0)
 
                     # Record tracking for trajectories
-                    if (frame_id - 1) % interval == 0:
+                    if (not args.no_track) and ((frame_id - 1) % interval == 0):
                         coordinate = [(float(x1) + float(x2)) / 2, float(y2)]
                         object_id = int(box.id[0]) if box.id is not None else -1
                         human_name = f"human{object_id}"
 
-                        if (object_id != -1):
+                        if (object_id != -1) and ((not args.restrict_area) or (cv2.pointPolygonTest(area, (coordinate[0], coordinate[1]), False) == True)):
                             if (object_id not in id_list):
                                 id_list.append(object_id)
                                 trajectory_dict[human_name] = {}
@@ -126,34 +171,40 @@ def main(args):
                             else:
                                 trajectory_dict[human_name]["trajectories"].append(coordinate)
             
-            if args.blur_all:
+            if (args.blur_all) and (not args.no_blur):
                 save_frame = cv2.GaussianBlur(save_frame, (args.blur_size, args.blur_size), 0)
 
-            if (frame_id - 1) % interval == 0:
+            if (not args.no_track) and ((frame_id - 1) % interval == 0):
+                if args.restrict_area:
+                    save_frame = cv2.polylines(save_frame, [area], True, (255, 255, 255), 1)
                 trajectory_video.write(save_frame)
-            output.write(save_frame)
+            if (not args.no_blur):
+                output.write(save_frame)
 
         # smooth trajectories
-        smooth_len = args.smooth_len // 2
-        for human_name in trajectory_dict:
-            trajectory = trajectory_dict[human_name]["trajectories"]
-            smooth_trajectory = []
-            for i in range(len(trajectory)):
-                x = 0
-                y = 0
-                count = 0
-                for j in range(max(0, i - smooth_len), min(len(trajectory), i + smooth_len)):
-                    x += trajectory[j][0]
-                    y += trajectory[j][1]
-                    count += 1
-                smooth_trajectory.append([x / count, y / count])
-            trajectory_dict[human_name]["trajectories"] = smooth_trajectory
+        if not args.no_track:
+            smooth_len = args.smooth_len // 2
+            for human_name in trajectory_dict:
+                trajectory = trajectory_dict[human_name]["trajectories"]
+                smooth_trajectory = []
+                for i in range(len(trajectory)):
+                    x = 0
+                    y = 0
+                    count = 0
+                    for j in range(max(0, i - smooth_len), min(len(trajectory), i + smooth_len)):
+                        x += trajectory[j][0]
+                        y += trajectory[j][1]
+                        count += 1
+                    smooth_trajectory.append([x / count, y / count])
+                trajectory_dict[human_name]["trajectories"] = smooth_trajectory
 
-        with open(trajectory_path, 'w') as f:
-            toml.dump(trajectory_dict, f)
+            with open(trajectory_path, 'w') as f:
+                toml.dump(trajectory_dict, f)
 
-        output.release()
-        trajectory_video.release()
+        if not args.no_blur:
+            output.release()
+        if not args.no_track:
+            trajectory_video.release()
         cap.release()
         print(f"\nFinished processing video: {video_file}")
         end_time = time.time()
---
+++ anonymization/define_area.py
@@ -0,0 +1,81 @@
+import cv2
+import yaml
+import os
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Define area of interest for tracking for a single video")
+    parser.add_argument("--video", type=str, required=True, help="Path to the video folder")
+    parser.add_argument("--area_path", default="./areas", type=str, help="Path to the area of interest folder")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.video):
+        print(f"ERROR: The video folder {args.video} does not exist.")
+        exit(1)
+    if not os.path.isfile(args.video):
+        print(f"ERROR: The video path {args.video} is not a file.")
+        exit(1)
+    if not os.path.exists(args.area_path):
+        os.makedirs(args.area_path)
+    elif not os.path.isdir(args.area_path):
+        print(f"ERROR: The area path {args.area_path} is not a folder.")
+        exit(1)
+    return args
+
+def select_area(frame):
+    window_name = "Select Area of Interest (At least 3 points)"
+    cv2.namedWindow(window_name)
+    area = []
+    cv2.imshow(window_name, frame)
+
+    def on_mouse(event, x, y, flags, param):
+        frame_copy = frame.copy()
+        if event == cv2.EVENT_LBUTTONDOWN:
+            area.append((x, y))
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            area.pop()
+        for point in area:
+            cv2.circle(frame_copy, point, 5, (0, 255, 0), -1)
+        cv2.imshow(window_name, frame_copy)
+
+    cv2.setMouseCallback(window_name, on_mouse, frame)
+
+    print("Select the area of interest by clicking on the image. Press 'q' to finish.")
+
+    while True:
+        key = cv2.waitKey(1) & 0xFF
+        # Press 'q' to quit
+        if key == ord("q"):
+            break
+
+    cv2.destroyWindow(window_name)
+
+    if len(area) < 3:
+        print("ERROR: At least 3 points are needed to define the area of interest.")
+        exit(1)
+    return area
+
+def main(args):
+    video_fname = os.path.basename(args.video)
+    video_name = os.path.splitext(video_fname)[0]
+    area_path = os.path.join(args.area_path, video_name + ".yaml")
+    cap = cv2.VideoCapture(args.video)
+    if not cap.isOpened():
+        print(f"ERROR: Cannot open video {args.video}")
+        exit(1)
+    ret, frame = cap.read()
+    if not ret:
+        print(f"ERROR: Cannot read video {args.video}")
+        exit(1)
+
+    area = select_area(frame)
+    cap.release()
+
+    with open(area_path, "w") as f:
+        yaml.dump(area, f)
+    print(f"Area of interest saved to {area_path}")
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
